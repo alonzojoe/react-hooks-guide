9815ab617d085fdee5feaa399dbbff4dbdb589fc
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-08-13 16:17:06 +0800

    feat: Create new page and contents for useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RootLayout from "@/Layouts/RootLayout";
+import HookUseState from "@/pages/HookUseState";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<RootLayout />}></Route>
+        <Route path="/" element={<RootLayout />}>
+          <Route path="/use-state" element={<HookUseState />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/HookUseState.jsx
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+
+const HookUseState = () => {
+  const [count, setCount] = useState(0);
+
+  const [name, setName] = useState("");
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    address: "",
+  });
+
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+
+    setFormData((prevData) => {
+      return {
+        ...prevData,
+        [name]: value,
+      };
+    });
+  };
+
+  return (
+    <>
+      <h2>
+        <code>useState</code>
+      </h2>
+      <p className="mt-3">
+        The <code>useState</code> hook is a React hook for managing state in
+        functional components in React. Syntax: <br />
+        <code>const [count, setCount] = useState(0)</code>
+        <br />
+        <code>count</code> is the value itself, and the second parameter{" "}
+        <code>setCount</code> in the array destructuring is the function that
+        updates the <code>count</code> state. Inside the <code>useState</code>{" "}
+        is the default value of the <code>count</code>. It also works with
+        arrays and objects.
+      </p>
+      <h3>
+        <i>useCase</i>
+      </h3>
+      <h3>
+        <code>Counter</code>
+      </h3>
+      <button
+        className="btn btn-light mx-3"
+        onClick={() => setCount((prev) => prev + 1)}
+      >
+        +
+      </button>
+      {count}
+      <button
+        className="btn btn-light mx-3"
+        onClick={() => setCount((prev) => prev - 1)}
+      >
+        -
+      </button>
+      <hr />
+      <h3>
+        <code>Two Way Binding Single Input</code>
+      </h3>
+      <div className="w-50">
+        <label className="mb-2">My Name is: {name}</label>
+        <input
+          type="text"
+          className="form-control form-control-sm"
+          value={name}
+          onChange={() => setName(event.target.value)}
+        />
+      </div>
+      <hr />
+      <h3>
+        <code>Two Way Binding Multiple Input (eg. Forms)</code>
+      </h3>
+      <div className="w-50">
+        <form>
+          <div className="mb-3">
+            <label for="name" className="form-label">
+              Name
+            </label>
+            <input
+              type="text"
+              value={formData.name}
+              onChange={changeHandler}
+              className="form-control"
+              name="name"
+              id="name"
+            />
+          </div>
+          <div className="mb-3">
+            <label for="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              value={formData.email}
+              onChange={changeHandler}
+              className="form-control"
+              name="email"
+              id="email"
+            />
+          </div>
+          <div className="mb-3">
+            <label for="address" className="form-label">
+              Address
+            </label>
+            <input
+              type="text"
+              value={formData.address}
+              onChange={changeHandler}
+              className="form-control"
+              name="address"
+              id="address"
+            />
+          </div>
+          <pre>
+            Output:
+            {JSON.stringify(formData)}
+          </pre>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default HookUseState;
