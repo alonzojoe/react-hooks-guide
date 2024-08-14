15dd802851dcdd5409a266a3dcbe6d297be90872
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-08-14 16:40:26 +0800

    chore: Add github link and active navlink indicator ::before adjustments

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,6 +1,7 @@
 import { useRef, useEffect, useState } from "react";
 import { Outlet, NavLink, Link } from "react-router-dom";
 import HookLogo from "@/assets/images/hook.png";
+import GitHubLogo from "@/assets/images/github.png";
 
 const RootLayout = () => {
   const headerRef = useRef(null);
@@ -27,24 +28,40 @@ const RootLayout = () => {
         ref={headerRef}
         className="p-2 bg-dark shadow top-0 position-fixed w-100 z-3"
       >
-        <button
-          className="btn btn-dark p-0 d-lg-none"
-          type="button"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#sidebar"
-          aria-controls="sidebar"
-        >
-          <i className="bx bx-menu-alt-left fs-1 border rounded route-link p-1"></i>
-        </button>
-        <Link to="/" className="text-decoration-none text-white align-middle">
-          <img src={HookLogo} alt="ReactHooks" width="50" height="50" />
-        </Link>
-        <Link
-          to="/"
-          className="mx-1 text-decoration-none text-white align-middle"
-        >
-          <span>React Hooks Guide</span>
-        </Link>
+        <div className="d-flex align-items-center justify-content-between">
+          <div>
+            {" "}
+            <button
+              className="btn btn-dark p-0 d-lg-none"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#sidebar"
+              aria-controls="sidebar"
+            >
+              <i className="bx bx-menu-alt-left fs-1 border rounded route-link p-1"></i>
+            </button>
+            <Link
+              to="/"
+              className="text-decoration-none text-white align-middle"
+            >
+              <img src={HookLogo} alt="ReactHooks" width="50" height="50" />
+            </Link>
+            <Link
+              to="/"
+              className="mx-1 text-decoration-none text-white align-middle"
+            >
+              <span>React Hooks Guide</span>
+            </Link>
+          </div>
+          <div className="mx-2">
+            <a
+              href="https://github.com/alonzojoe/react-hooks-guide"
+              target="_blank"
+            >
+              <img src={GitHubLogo} alt="ReactHooks" width="35" height="35" />
+            </a>
+          </div>
+        </div>
       </h1>
       <div className="row" style={{ marginTop: headerHeight }}>
         <div
