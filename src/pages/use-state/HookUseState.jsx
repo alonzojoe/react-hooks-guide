4fc97b21ff0ed29ce7a91ea3bcb2a5a35a6b1603
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-08-20 15:48:58 +0800

    feat: refactor useState page

--- src/pages/use-state/HookUseState.jsx
+++ src/pages/use-state/HookUseState.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Input from "@/components/UI/Input";
+import CodeEditor from "@uiw/react-textarea-code-editor";
 
 const HookUseState = () => {
   const [count, setCount] = useState(0);
@@ -32,58 +33,106 @@ const HookUseState = () => {
         The <code>useState</code> hook is a React hook for managing state in
         functional components. It allows you to add state variables to your
         functional components. Here’s how it works:
-        <br />
-        <br />
-        <strong>Syntax:</strong> <br />
-        <code>const [count, setCount] = useState(0)</code>
-        <br />
-        <br />-{" "}
-        <strong>
-          <code>count</code>
-        </strong>
-        : This is the current value of the state variable.
-        <br />-{" "}
-        <strong>
-          <code>setCount</code>
-        </strong>
-        : This is the function used to update the state variable.
-        <br />- Inside the <code>useState</code> call, <code>0</code> is the
-        default value of <code>count</code>.
-        <br />
-        <br />
-        The <code>useState</code> hook can also be used with arrays and objects.
-        For example:
-        <br />
-        <code>
-          {`const [user, setUser] = useState({ name: 'John', age: 30 })`}
-        </code>
-        <br />
-        In this case, <code>user</code> is an object containing{" "}
-        <code>name</code> and <code>age</code>, and <code>setUser</code> is used
-        to update this object.
-        <br />
-        <br />
-        Updating state with the previous value: When the new state depends on
-        the previous state, you should use a callback function with{" "}
-        <code>setCount</code>. This ensures that you’re working with the most
-        recent state value, especially in cases where state updates might be
-        batched or asynchronous. Here’s how you use it:
-        <br />
-        <br />
-        <strong>Example:</strong>
-        <br />
-        <code>setCount(prevCount =&gt; prevCount + 1);</code>
-        <br />
-        In this example, <code>prevCount</code> represents the previous state
-        value. By using this callback function, you ensure that the new state is
-        based on the most recent value of <code>count</code>, which is important
-        when multiple state updates might occur simultaneously or in quick
-        succession.
-        <br />
-        <br />
-        This pattern is especially useful in scenarios where you have multiple
-        state updates that need to be processed in a specific order or when the
-        state update is dependent on the current state value.
+        <span className="d-block my-3">Syntax:</span>
+        <p>
+          <CodeEditor
+            value={`const [count, setCount] = useState(0);`}
+            language="jsx"
+            placeholder="Please enter JSX code."
+            onChange={(evn) => setCode(evn.target.value)}
+            padding={0}
+            data-color-mode="dark"
+            style={{
+              backgroundColor: "#161B22",
+              borderRadius: "5px",
+              pointerEvents: "none",
+              fontSize: "15px",
+              fontFamily:
+                "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+            }}
+          />
+        </p>
+        <ul className="mt-3">
+          <li>
+            <code>count()</code> This is the current value of the state
+            variable.
+          </li>
+          <li>
+            <code>setCount()</code> This is the function used to update the
+            state variable.
+          </li>
+          <li>
+            Inside the <code>useState()</code> hook, <code>0</code> is the
+            default value of <code>count</code>.
+          </li>
+        </ul>
+        <p>
+          The <code>useState</code> hook can also be used with arrays and
+          objects.
+        </p>
+        <span className="d-block my-3">For example:</span>
+        <p>
+          <CodeEditor
+            value={`const [user, setUser] = useState({ name: 'John', age: 30 })`}
+            language="jsx"
+            placeholder="Please enter JSX code."
+            onChange={(evn) => setCode(evn.target.value)}
+            padding={0}
+            data-color-mode="dark"
+            style={{
+              backgroundColor: "#161B22",
+              borderRadius: "5px",
+              pointerEvents: "none",
+              fontSize: "15px",
+              fontFamily:
+                "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+            }}
+          />
+        </p>
+        <p>
+          In this case, <code>user</code> is an object containing{" "}
+          <code>name</code> and <code>age</code>, and <code>setUser</code> is
+          used to update this object.
+        </p>
+        <p>
+          Updating state with the previous value: When the new state depends on
+          the previous state, you should use a callback function with{" "}
+          <code>setCount</code>. This ensures that you’re working with the most
+          recent state value, especially in cases where state updates might be
+          batched or asynchronous. Here’s how you use it:
+        </p>
+        <span className="d-block my-3">Example:</span>
+        <p>
+          <CodeEditor
+            value={`setCount((prevCount) => prevCount + 1)`}
+            language="jsx"
+            placeholder="Please enter JSX code."
+            onChange={(evn) => setCode(evn.target.value)}
+            padding={0}
+            data-color-mode="dark"
+            style={{
+              backgroundColor: "#161B22",
+              borderRadius: "5px",
+              pointerEvents: "none",
+              fontSize: "15px",
+              fontFamily:
+                "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+            }}
+          />
+        </p>
+        <p>
+          In this example, <code>prevCount</code> represents the previous state
+          value. By using this callback function, you ensure that the new state
+          is based on the most recent value of <code>count</code>, which is
+          important when multiple state updates might occur simultaneously or in
+          quick succession.
+        </p>
+        <p>
+          {" "}
+          This pattern is especially useful in scenarios where you have multiple
+          state updates that need to be processed in a specific order or when
+          the state update is dependent on the current state value.
+        </p>
       </p>
 
       <h3>
